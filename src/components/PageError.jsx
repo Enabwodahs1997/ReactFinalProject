5c711e3a089eefdb1c87433184c7f3bf956e1495
Enabwodahs1997 <sesolstice1997@gmail.com>
2026-04-13 09:19:33 -0600

    made the droid image work by changing the path to the image in PageError.jsx and moving the image to the src/assets folder. Also updated the commit message to reflect these changes.

--- src/components/PageError.jsx
+++ src/components/PageError.jsx
@@ -7,7 +7,7 @@ export default function NotFound() {
             <h1>404 - Page Not Found</h1>
             <p>Sorry, the droid you are looking for does not exist.</p>
             <img
-                src="/droid-buddy.svg"
+                src="/assets/droid-buddy.png"
                 alt="Friendly sci-fi droid illustration"
                 className="page-error-image"
                 />
